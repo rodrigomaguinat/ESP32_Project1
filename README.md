ee078ba9ac9347b1e5f9de97cd91e82060e29b79
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2022-09-08 15:43:49 -0500

    Modulo, Incrementos, Decrementos

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #### Prototype and simulation
 
-[![Captura-de-pantalla-2022-09-08-123907.jpg](https://i.postimg.cc/L66tRbCt/Captura-de-pantalla-2022-09-08-123907.jpg)](https://postimg.cc/0rFM0cqj)
+[![Captura-de-pantalla-2022-09-08-154117.jpg](https://i.postimg.cc/FRZYkSC1/Captura-de-pantalla-2022-09-08-154117.jpg)](https://postimg.cc/vg4Yp4sd)
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,67 @@
 #include <Arduino.h>
 
-// Variable de tipo boleano, solo puede almacenar verdadero o falso (true, false)
-bool myBool = true;
-bool myBool2 = false;
-
-// Variable de tipo byte (1 byte)
-byte myByte = 1;
-byte myByte2;
-
-// Variable de tipo caracter (1 byte)
-char myChar = 'E';
-char myChar2;
-
-// Variable de tipo entero (2 bytes)
-int myInt = 1;
-int myInt2;
-
-// Variable de tipo decimal simple (4 bytes) hasta 6 decimales
-float myFloat = 1.0;
-float myFloat2;
-
-// Variable de tipo entero largo (4 bytes) valores enteros
-long myLong = 1;
-long myLong2;
-
-// Variable de tipo doble decimal (8 bytes) hasta 14 decimales
-double myDouble = 1.0;
-double myDouble2;
-
-// Variable de tipo cadena de caracteres
-String myString = "Hola mundo";
-String myString2;
+/* La variable bool solo puede guardar valores VERDADERO o FALSO (true - false) */
+bool var = true;
+bool var2 = false;
+ 
+/****** OPERADOR MODULO ******/
+/*             %             */
 
 
-// Variable uint8_t
-uint8_t my_uint_8 = 12;
+/* Se puede declarar varias variables con un solo tipo de variable, 
+   solo hay que separar los nombres de las variables por comas. */
+     int       a=3,    b, c=0, d=0; 
+/*    ^         ^      ^  
+    Tipo      Nombre  Nombre  */
 
 
 void setup() {
-  myByte2 = 117;
-  Serial.begin(9600);
+  Serial.begin(9600);     // Inicialización de comunicación serial.
+  b = 3;                  // Asignación del valor de 3 en la variable b.
+
+  Serial.print("3%3: ");  // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(a%b);    // Envio de la operación modulo de a entre b y despues un salto de linea.
+
+  a = 4;                  // Asignación del valor de 4 en la variable a.
+  Serial.print("4%3: ");  // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(a%b);    // Envio de la operación modulo de a entre b y despues un salto de linea.
+
+  a = 5;                  // Asignación del valor de 5 en la variable a.
+  Serial.print("5%3: ");  // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(a%b);    // Envio de la operación modulo de a entre b y despues un salto de linea.
+
+  /* Se les asigno este valor de forma que en el 
+     incremento o decremento empezara desde 0. */
+  a = 0;                  // Asignación del valor de 0 en la variable a.
+  b = 0;                  // Asignación del valor de 0 en la variable b.
 }
 
 void loop() {
-  Serial.print("Byte : ");
-  Serial.println(myByte2);
-  Serial.print("Espacio de memoria de varible int : ");
-  Serial.println(sizeof(myInt));
-  Serial.print("Espacio de memoria de varible uint8_t : ");
-  Serial.println(sizeof(my_uint_8));
 
-  delay(2000);
+  // Operación alternativa para realizar un incremento. =>   a = a + 1;
+
+  a++;                                      // Realizamos un incremento en 1 a la variable a.
+  Serial.print("Incremento de A: ");        // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(a);                        // Envio del valor de la variable a por comunicación serial con salto de linea.
+
+  // Operación alternativa para realizar un decremento. =>   b = b - 1;
+
+  b--;                                      // Realizamos un decremento en 1 a la variable b.
+  Serial.print("Decremento de B: ");        // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(b);                        // Envio del valor de la variable b por comunicación serial con salto de linea.
+
+  // Operación alternativa para realizar un incremento. =>   c = c + 2;
+
+  c+=2;                                     // Realizamos un incremento en 2 a la variable c.
+  Serial.print("Incremento de C en 2: ");   // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(c);                        // Envio del valor de la variable b por comunicación serial con salto de linea.
+
+  // Operación alternativa para realizar un decremento. =>   d = d - 4; 
+
+  d-=4;                                     // Realizamos un incremento en 4 a la variable d.
+  Serial.print("Decremento de D en 4:");    // Envio de una cadena de caracteres por comunicación serial.
+  Serial.println(d);                        // Envio del valor de la variable d por comunicación serial con salto de linea.
+
+
+  delay(1000);                              // Esperamos 1 segundo en esta linea.
 }
