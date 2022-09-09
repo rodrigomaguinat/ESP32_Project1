d53f1bf48f3c2b02e3289d21bc64fb4273e949b9
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2022-09-09 12:57:24 -0500

    Clase 3 - Ejemplo 2 - Entrada y Salida Digital

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #### Prototype and simulation
 
-[![Captura-de-pantalla-2022-09-09-105359.jpg](https://i.postimg.cc/rFMQmVWz/Captura-de-pantalla-2022-09-09-105359.jpg)](https://postimg.cc/6TbdbNMN)
+[![Captura-de-pantalla-2022-09-09-122141.jpg](https://i.postimg.cc/tgyPvhwR/Captura-de-pantalla-2022-09-09-122141.jpg)](https://postimg.cc/QF4HHWjv)
+[![Captura-de-pantalla-2022-09-09-122434.jpg](https://i.postimg.cc/SxKjnDBM/Captura-de-pantalla-2022-09-09-122434.jpg)](https://postimg.cc/qz9pYXMJ)
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,26 @@
 #include <Arduino.h>
 
-#define PinADC 34
-#define Resolucion 3.3/4095     // Resolucion de ADC de 12bits - 4096 combinaciones 
-//#define Resolucion 3.3/2047   // Resolucion de ADC de 11bits - 2048 combinaciones
-//#define Resolucion 3.3/1023   // Resolucion de ADC de 10bits - 1024 combinaciones
-//#define Resolucion 3.3/511    // Resolucion de ADC de 9bits - 512 combinaciones
+/***************************** NOTA *****************************
+ * Las terminales de la 34 a la 39 no tienen resistencias Pull-Up
+ * ni Pull-Down y no pueden ser usadas como salidas.
+ */
 
-int rADC = 0;
-// int voltaje = 0;
+#define PinIN 18
+#define PinOUT 2
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(PinOUT, OUTPUT);
 
-  analogReadResolution(12); // Resolucion del ADC, Puede tomar valores del 9 al 12
-  //analogSetAttenuation(ADC_11db); // Atenuacion del ADC, puede tomar valores ADC_0db, ADC_2_5db, ADC_6db, ADC_11db
-  analogSetPinAttenuation(PinADC, ADC_11db);
-
-  /***************************************
-   * La atenuación de 0 dB (ADC_0db) proporciona un voltaje de escala completa de 1,1 V
-   * La atenuación de 2,5 dB (ADC_2_5db) proporciona un voltaje de escala completa de 1,5 V
-   * La atenuación de 6 dB (ADC_6db) proporciona un voltaje de escala completa de 2,2 V
-   * La atenuación de 11 dB (ADC_11db) proporciona un voltaje de escala completa de 3,9 V (Nota)
-   * 
-   * A una atenuación de 11 dB, el voltaje máximo está limitado por VDDA, no por el voltaje de escala completa.
-   **************************************/
-
-
-  //https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html
-
+  //pinMode(PinIN, INPUT);
+  pinMode(PinIN, INPUT_PULLDOWN);
+  //pinMode(PinIN, INPUT_PULLUP);
 }
 
 void loop() {
-  
-  rADC = analogRead(PinADC);
 
-  Serial.print("Valor: ");
-  Serial.print(rADC);
-  Serial.print(" --> ");
-  Serial.print("Voltaje: ");
-  Serial.println(rADC*Resolucion,2);
-  
-  delay(500);
+  if(digitalRead(PinIN)){             // Lectura del estado de la entrada PinIN (si se presiona el botón)
+    digitalWrite(PinOUT, HIGH);       // Establece el valor en Alto en la salida PinOUT (enciende la luz)
+  }else{
+    digitalWrite(PinOUT, LOW);        // Establece el valor en Baja en la salida PinOUT 
+  }
 }
