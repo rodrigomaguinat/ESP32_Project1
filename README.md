d6cee732bcdb376ebd599bdb6e6d220e2b1d23e4
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2022-09-11 11:31:56 -0500

    Clase 3 - Ejemplo 3 - DAC a ADC

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 #### Prototype and simulation
 
-[![Captura-de-pantalla-2022-09-09-122141.jpg](https://i.postimg.cc/tgyPvhwR/Captura-de-pantalla-2022-09-09-122141.jpg)](https://postimg.cc/QF4HHWjv)
-[![Captura-de-pantalla-2022-09-09-122434.jpg](https://i.postimg.cc/SxKjnDBM/Captura-de-pantalla-2022-09-09-122434.jpg)](https://postimg.cc/qz9pYXMJ)
+[![Captura-de-pantalla-2022-09-11-083750.jpg](https://i.postimg.cc/kXBdh9c7/Captura-de-pantalla-2022-09-11-083750.jpg)](https://postimg.cc/tYQfYKwc)
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,47 @@
 #include <Arduino.h>
 
-/***************************** NOTA *****************************
- * Las terminales de la 34 a la 39 no tienen resistencias Pull-Up
- * ni Pull-Down y no pueden ser usadas como salidas.
- */
+/***************************************
+ * Tenemos dos salidas digitales a analógicas (DAC) 
+ * Son las terminales 25 y 26, tiene un DAC de 8 bits - 256 combinaciones
+ * tenemos del 0 al 255  
+ * donde 0 es 0v y 255 es 3.3v
+ ***************************************/
 
-#define PinIN 18
-#define PinOUT 2
+double j = 0;
+#define A 127           // Amplitud
+#define f 50            // Frecuencia
+#define B 128
+#define Salida 25
+
+#define PinADC 34
+#define Resolucion 3.3/4095   // Resolucion de ADC de 12 bits - 4096 combinaciones
+
+int rADC = 0;
+int voltaje = 0;
 
 void setup() {
-  pinMode(PinOUT, OUTPUT);
+  Serial.begin(9600);
 
-  //pinMode(PinIN, INPUT);
-  pinMode(PinIN, INPUT_PULLDOWN);
-  //pinMode(PinIN, INPUT_PULLUP);
+  analogReadResolution(12); // Resolucion del ADC, Puede tomar valores del 9 al 12
+  //analogSetAttenuation(ADC_11db); // Atenuacion del ADC, puede tomar valores ADC_0db, ADC_2_5db, ADC_6db, ADC_11db
+  analogSetPinAttenuation(PinADC, ADC_11db);
 }
 
 void loop() {
 
-  if(digitalRead(PinIN)){             // Lectura del estado de la entrada PinIN (si se presiona el botón)
-    digitalWrite(PinOUT, HIGH);       // Establece el valor en Alto en la salida PinOUT (enciende la luz)
-  }else{
-    digitalWrite(PinOUT, LOW);        // Establece el valor en Baja en la salida PinOUT 
-  }
-}
+  /* Nivel de CD */
+  dacWrite(26, 255);  // Establecer un voltaje en la terminal 26.
+
+  rADC = analogRead(PinADC);
+
+  Serial.print("Valor: ");
+  Serial.print(rADC);
+  Serial.print(" --> ");
+  Serial.print("Voltaje: ");
+  Serial.println(rADC*Resolucion,2);
+  
+   /* Señal senoidal */
+ for(j = 0; j<360; j+=0.00120){
+   dacWrite(Salida, A * sin(2*PI*f*(j*PI/180)) + B);
+ }
+} 
