6a864c86f9a3ab369641ec5faf7205383abb9818
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2022-09-13 16:39:44 -0500

    Clase 4 - Ejemplo 1 - Salida Analogica en Funcion Seno con Potenciometro

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+#### Notes
+
+- La Amplitud no está variando, lo que varía en el Osciloscópio es la Frecuencia
+- La Frecuencia lo controlamos con el Potenciómetro
+
 #### Prototype and simulation
 
-[![Captura-de-pantalla-2022-09-12-181010.jpg](https://i.postimg.cc/Rhty90Dn/Captura-de-pantalla-2022-09-12-181010.jpg)](https://postimg.cc/n9H0GnZH)
+[![Captura-de-pantalla-2022-09-13-162330.jpg](https://i.postimg.cc/cCqspSxh/Captura-de-pantalla-2022-09-13-162330.jpg)](https://postimg.cc/LYTKPGZg)
+[![Captura-de-pantalla-2022-09-13-162737.jpg](https://i.postimg.cc/LscKX3c6/Captura-de-pantalla-2022-09-13-162737.jpg)](https://postimg.cc/1grY7wRL)
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,45 @@
 #include <Arduino.h>
 
 /********************************************************
- NOTA
- Las terminales de la 34 a la 39 no tienen resistencias 
- Pull-Up ni Pull-Down y no pueden ser usadas como salidas
+ - Tenemos dos salidas digitales a analógicas (DAC)
+ - Son las terminales 25 y 26, tiene un DAC de 8 bits - 256 combinaciones
+ - Tenemos del 0 al 255 donde 0 es 0V y 255 es 3.3V
  ********************************************************/
 
-#define Interrupcion 23
-#define Salida 2
- 
-volatile boolean toggle = true;
-volatile unsigned long tiempoDeInterrupcionAnterior = 0;
-#define tiempoDeRebote 400
+// Configuracion ADC
+#define PinADC 34
+#define Resolucion 3.3V/4095    // Resolucion de ADC de 12 bits - 4096 combinaciones
 
-/* Declaración de la función de interrupción */
-// IRAM_ATTR : Definimos que se tratará de una Interrupción Externa
-void IRAM_ATTR funcionDeInterrupcion();
+// Configuracion de Onda Seno
+double j = 0;
+#define A 127                   // Amplitud
+#define B 128
+#define Salida 25
+int freq = 50;                  // Frecuencia
+
+// Variables para el ADC
+int rADC = 0;
+int voltaje = 0;
+
 
 void setup() {
-
   Serial.begin(9600);
-  pinMode(Salida, OUTPUT);
-  pinMode(Interrupcion, INPUT_PULLUP);                            // Configuración de resistencia Pull-Up 
-  //pinMode(Interrupcion, INPUT_PULLDOWN);                        // Configuración de resistencia Pull-Down 
-  attachInterrupt(digitalPinToInterrupt(Interrupcion), funcionDeInterrupcion, FALLING);  // Flanco de bajada 
-  //attachInterrupt(digitalPinToInterrupt(Interrupcion), funcionDeInterrupcion, RISING); // Flanco de subida 
-  //attachInterrupt(digitalPinToInterrupt(Interrupcion), funcionDeInterrupcion, CHANGE); // Detecta ambos flancos
 
+  analogReadResolution(12);     // Resolucion del ADC. Puede tomar valore del 9 al 12
+  //analogSetAttenuation(ADC_11db);   
+  //Atenuacion del ADC, puede tomar valores ADC_0db, ADC_2_5db, ADC_6db, ADC_11db
+  analogSetPinAttenuation(PinADC,ADC_11db);
 }
 
 void loop() {
-
-  Serial.println("Mensaje");
-  delay(1000);
-
-  if (toggle){
-    Serial.println("Estoy en el if");
-  }
-
-  // Instrucción 1
-  // Instrucción 2
-  // Instrucción 3
+  freq = analogRead(PinADC);
+  Serial.println(freq);
   
-}
+  // Nivel de CD
+  //dacWrite(25,117);           // Establecer un voltaje en la terminal 25
 
-/* Definición de la función de interrupción */
-void IRAM_ATTR funcionDeInterrupcion() {
-  if(millis() - tiempoDeInterrupcionAnterior > tiempoDeRebote){
-
-    digitalWrite(Salida, toggle);
-    Serial.println("Interrupcion");
-    toggle ^= true; 
-    //toggle = toggle^true    
-    // togglear : ir de un falso a un verdadero (prender o apagar LED); cambiar de estado
-    
-    tiempoDeInterrupcionAnterior = millis();
-  }                                                            
-}
+  // Señal Senoidal
+  for (j=0; j<20; j+=0.00120){
+    dacWrite(Salida, A * sin(2 * PI * freq * (j * PI/180)) + B);
+  }
+} //https://www.geogebra.org/m/DcUsxQz4
