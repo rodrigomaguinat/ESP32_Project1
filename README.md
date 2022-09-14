e90548b63171718335ed0db22538ea4a855b072d
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2022-09-14 17:25:55 -0500

    Clase 4 - Ejemplo 2 - Interrupciones Timer

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 #### Notes
 
-- La Amplitud no está variando, lo que varía en el Osciloscópio es la Frecuencia
-- La Frecuencia lo controlamos con el Potenciómetro
+- En el simulador no se aprecia bien con respecto al tiempo, debido a que el LED debe parpadear intercaladamente cada segundo
+
+ 		timerAlarmWrite(timer0, 1000000, true);
+
+	El segundo parametro es con respecto a la Frecuencia, lo que nos da cada 1 segundo
+
+- Cuando cambiamos a 10 mili seg y lo probamos en nuestro ESP32, notaremos que el LED pareciera que se mantiene continuamente encendido, esto es porque la ejecución a esa frecuencia no se aprecia el encendido y apagado
+		timerAlarmWrite(timer0, 10000, true);
+
 
 #### Prototype and simulation
 
-[![Captura-de-pantalla-2022-09-13-162330.jpg](https://i.postimg.cc/cCqspSxh/Captura-de-pantalla-2022-09-13-162330.jpg)](https://postimg.cc/LYTKPGZg)
-[![Captura-de-pantalla-2022-09-13-162737.jpg](https://i.postimg.cc/LscKX3c6/Captura-de-pantalla-2022-09-13-162737.jpg)](https://postimg.cc/1grY7wRL)
+[![Captura-de-pantalla-2022-09-13-173941.jpg](https://i.postimg.cc/3wj216VD/Captura-de-pantalla-2022-09-13-173941.jpg)](https://postimg.cc/McTcKsWW)
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,60 @@
 #include <Arduino.h>
 
-/********************************************************
- - Tenemos dos salidas digitales a analógicas (DAC)
- - Son las terminales 25 y 26, tiene un DAC de 8 bits - 256 combinaciones
- - Tenemos del 0 al 255 donde 0 es 0V y 255 es 3.3V
- ********************************************************/
+#define Salida 2
 
-// Configuracion ADC
-#define PinADC 34
-#define Resolucion 3.3V/4095    // Resolucion de ADC de 12 bits - 4096 combinaciones
+volatile bool toggle = true;
+void IRAM_ATTR onTimer();   // Declarar la función de interrupción
 
-// Configuracion de Onda Seno
-double j = 0;
-#define A 127                   // Amplitud
-#define B 128
-#define Salida 25
-int freq = 50;                  // Frecuencia
-
-// Variables para el ADC
-int rADC = 0;
-int voltaje = 0;
+hw_timer_t *timer0 = NULL;   // Declarar un apuntador de tipo hw_timer_t
 
 
 void setup() {
-  Serial.begin(9600);
 
-  analogReadResolution(12);     // Resolucion del ADC. Puede tomar valore del 9 al 12
-  //analogSetAttenuation(ADC_11db);   
-  //Atenuacion del ADC, puede tomar valores ADC_0db, ADC_2_5db, ADC_6db, ADC_11db
-  analogSetPinAttenuation(PinADC,ADC_11db);
+  Serial.begin(9600);                             // Configuración de comunicación serial 
+
+  pinMode(Salida, OUTPUT);                        // Configuración de la terminal Salida como salida
+  // Activando algo, puede ser un LED, motor, etc
+
+  timer0 = timerBegin(0, 80, true);                // Inicializamos el timer
+  /*******************************
+   * 1.- El primer parámetro es el timer que vamos a utilizar, puede tomar valores de 0 a 3 (hay 4 Timer), todos los timer son de 64 bits
+   * 2.- El segundo parámetro es el valor del preescaler, el prescaler es de 16 bits lo cual puede tomar valores de 0 a 65536 ('onTimer' es el nombre de mi Función de Interrupción)
+   * 3.- El tercer parámetro indica si el contador cuenta hacia arriba o hacia abajo, puede tomar valores booleanos, true o false respectivamente.
+   ******************************/
+
+  timerAttachInterrupt(timer0, &onTimer, true);    // Indicamos que función de interrupción se ejecutara.
+  /********************************
+   * 1.- El primer parámetro es el apuntador '*timer0'
+   * 2.- El segundo parámetro es la dirección de memoria de la función de interrupción que se ejecutara.
+   * 3.- El tercer parámetro indica si la interrupción a generar es de flanco o de nivel, toma valores booleanos, true y false respectivamente.
+   ********************************/
+
+  timerAlarmWrite(timer0, 10000, true);          // Establecer el limite del contador
+  /********************************
+   * 1.- El primer parámetro es el apuntador *timer0.
+   * 2.- El segundo parámetro es el valor al que tiene que llegar el contador para ejecutar la función de interrupción. 
+            (1000000 :  1      seg -    1 parpadeo  en 1 seg)  - cuando es muy lento, una señal es dificil de ser capturada
+            (  10000 : 10 mili seg -  100 parpadeos en 1 seg)
+            (   1000 : 10 kilo seg - 1000 parpadeos en 1 seg)  // podemos tener esto, porque tenemos una precision de 1 micro seg
+   * 3.- El tercer argumento indica si el contador debe volver a cero o no (o reiniciar)
+   *******************************/
+
+  timerAlarmEnable(timer0);                        // Habilitamos la interrupción del timer.
+
+  /* Tambien podemos deshabilitar el timer con */
+  //timerAlarmDisable(timer);
+  
 }
 
 void loop() {
-  freq = analogRead(PinADC);
-  Serial.println(freq);
-  
-  // Nivel de CD
-  //dacWrite(25,117);           // Establecer un voltaje en la terminal 25
+    digitalWrite(Salida, toggle);                 // Que el digitalWrite de Salida cambie o se togglee
+}
 
-  // Señal Senoidal
-  for (j=0; j<20; j+=0.00120){
-    dacWrite(Salida, A * sin(2 * PI * freq * (j * PI/180)) + B);
-  }
-} //https://www.geogebra.org/m/DcUsxQz4
+
+// Función de interrupción.
+void IRAM_ATTR onTimer() {
+  toggle ^= true;                                // El toogle va a cambiar (invierte el valor)
+  //toggle =toggle xor true
+  
+  // cada 1 seg cambia el valor del toggle
+}
